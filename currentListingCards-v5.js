dd6ac5fdbb5ff0b0f880961537062eeac3924b8f
Mark Harle <markharle@gmail.com>
2025-08-07 09:43:03 -0500

    Update currentListingCards-v5.js
    Re-test

--- currentListingCards-v5.js
+++ currentListingCards-v5.js
@@ -33,8 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
     return 'Price upon request';
   }
   
-  // Fetch the JSON data   
-  //fetch('https://script.google.com/macros/s/AKfycbzaUVXyP2jt1eH7QQUt2pFlRE5b9jJVaqHC3d2TCuIyL58jDocQ5jWrUgd2m9OCEZbG/exec')
+  // Fetch the JSON data     
   fetch('https://script.google.com/macros/s/AKfycbzrauZh3iI4ZYGekexd9Z0sO0E1bpL34ohwOAM6TzrrEg-CMBg1oWpGE6SW4vsY-Q0x/exec?function=doGetListings')
     .then(response => {
       if (!response.ok) {
@@ -120,4 +119,5 @@ document.addEventListener('DOMContentLoaded', function() {
       cardDeckOverlay.style.display = 'none';
     });
 });
+
 
