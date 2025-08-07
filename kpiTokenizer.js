747d31750a06c3bb424cbefbd64bac708216d715
Mark Harle <markharle@gmail.com>
2025-08-07 13:49:25 -0500

    Update kpiTokenizer.js
    Change fetch to GitHub via Google App Script

--- kpiTokenizer.js
+++ kpiTokenizer.js
@@ -2,7 +2,8 @@
 
 async function fetchAndDisplayTokens() {
   // Updated URL to fetch KPI data from the Google Apps Script web app
-  const url = 'https://script.google.com/macros/s/AKfycbwOWyF6xrdhVAfTzNFF_K_7UzrmUkypkekl6EmnqyEXGjEbsaPOKc18QZ6SsrIl9-5J/exec?function=doGetKPI';
+  // const url = 'https://script.google.com/macros/s/AKfycbwOWyF6xrdhVAfTzNFF_K_7UzrmUkypkekl6EmnqyEXGjEbsaPOKc18QZ6SsrIl9-5J/exec?function=doGetKPI';
+  const url = 'https://script.google.com/macros/s/AKfycbzrauZh3iI4ZYGekexd9Z0sO0E1bpL34ohwOAM6TzrrEg-CMBg1oWpGE6SW4vsY-Q0x/exec?function=doGetKPI';
   
   // An object to hold our target elements. This makes the code cleaner.
   const elements = {
@@ -82,4 +83,5 @@ document.addEventListener('DOMContentLoaded', fetchAndDisplayTokens);
 
 // If you are using <script defer>, you can just call it directly:
 // fetchAndDisplayTokens();
+
 
